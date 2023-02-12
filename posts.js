a0d4041b42ce1775058f2757c41c57c874060228
polymahh <otman.elkantaoui@gmail.com>
2023-02-12 23:02:56 +0100

    added detail page

---
+++ posts.js
@@ -0,0 +1,191 @@
+const posts = [
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post1.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post2.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post3.png",
+  },
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post4.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post5.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post6.png",
+  },
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post7.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post8.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post9.png",
+  },
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post9.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post8.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post7.png",
+  },
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post6.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post5.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post4.png",
+  },
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post3.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post2.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post1.png",
+  },
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post1.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post2.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post3.png",
+  },
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post4.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post5.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post6.png",
+  },
+  {
+    title: "Contact Plus",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post7.png",
+  },
+  {
+    title: "Commodo sodales",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post8.png",
+  },
+  {
+    title: "Purus porta",
+    date: "le 29-12-2022 12:08",
+    preview:
+      "Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris. Aenean scelerisque,",
+    image: "images/post9.png",
+  },
+];
--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const menuBtn = document.querySelector(".menu-icon");
 const closeMenu = document.querySelector(".close-menu");
 const menuNav = document.querySelector(".menu-nav");
 const navLinks = document.querySelectorAll(".nav-link");
+const questions = document.querySelectorAll(".question");
+const postsContainer = document.querySelector(".content");
+const pagesNav = document.querySelector(".pagination");
 
 // toggle open and close menu
 const togglemenu = () => {
@@ -26,3 +29,61 @@ navLinks.forEach((link) =>
     link.classList.add("active-link");
   })
 );
+// accordion for the questions
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    let panel = question.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      panel.style.padding = `0 18px`;
+      question.childNodes[0].classList.remove("active");
+    } else {
+      panel.style.maxHeight = `${panel.scrollHeight}px`;
+      panel.style.padding = `1rem 18px`;
+      question.childNodes[0].classList.add("active");
+    }
+  });
+});
+// pagination and showing posts based on page
+
+let cards = posts.map((post) => {
+  const card = document.createElement("div");
+  const title = document.createElement("h3");
+  const date = document.createElement("span");
+  const image = document.createElement("img");
+  const paragraph = document.createElement("p");
+  const more = document.createElement("a");
+  const dateicon = document.createElement("span");
+  const datetext = document.createElement("p");
+  card.classList.add("card");
+  title.classList.add("title");
+  date.classList.add("date");
+  paragraph.classList.add("paragraph");
+  more.classList.add("link");
+  dateicon.classList.add("date-icon");
+  datetext.innerText = post.date;
+  date.append(dateicon, datetext);
+  title.innerText = post.title;
+  image.src = post.image;
+  paragraph.innerText = post.preview;
+  more.innerText = "Read more ->";
+
+  card.append(title, date, image, paragraph, more);
+  return card;
+});
+
+console.log(cards);
+const postsRange = 9; //number of posts per page
+// creating pages buttons
+const pages = Math.ceil(cards.length / 9);
+// for(let i =0;i<=pages ;i++){
+
+// }
+
+const pagination = (page) => {
+  const start = page * postsRange - postsRange;
+  const end = page * postsRange - 1;
+  const currentPosts = cards.slice(start, end);
+  postsContainer.append(...currentPosts);
+  console.log(currentPosts);
+};
